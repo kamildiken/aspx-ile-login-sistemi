c5c9e4b44320deca8dcbb25822b3fefa69e7d63a
Kamil Diken <kamildiken8@gmail.com>
2024-11-06 02:33:07 +0300

    asp ile login sistemi
    Asp.Net ile veritabanı bağlantılı login sistemi

---
+++ loginforweb/loginforweb/index.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="index.aspx.cs" Inherits="loginforweb.index" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Login Services</title>
+    <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+  <div class="login">
+      <h2>Giriş Yap</h2>
+       <form id="form1" runat="server">
+
+          <div class="k">
+            <asp:Label ID="Label1" runat="server" Text="Kullanıcı Adı"></asp:Label>
+              <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+          </div>
+
+          <div class="k">
+              <asp:Label ID="Label2" runat="server" Text="Şifre"></asp:Label>
+              <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+          </div>
+            <%--GİRİŞ BUTONU--%>
+          <asp:Button ID="Button1" runat="server" Text="Giriş Yap" CssClass="button" OnClick="Button1_Click" />
+
+      
+           <%--HATA MESAJI--%>
+          <br/> <asp:Label ID="Label3" runat="server" class="hata" ><strong>Hata!</strong> Kullanıcı Adı veya Şifre Hatalı!</asp:Label>
+           </form>
+  </div>
+   
+</body>
+</html>
---
+++ loginforweb/loginforweb/index.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace loginforweb
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // SAYFA BAŞLADIĞIINDA HATA MESAJININ GÖRÜNMEMESİ İÇİN
+            Label3.Visible = false;
+        }
+        // BAĞLANTI KURMA
+        SqlConnection baglanti = new SqlConnection("Veritabanı bağlantısını buraya yapıştırın");
+        SqlCommand komut;
+        SqlDataReader oku;
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            // tablodan verileri çekiyoruz
+            komut = new SqlCommand("select * from TABLOADI where kullaniciadi=@k1 and sifre=@k2", baglanti);
+            komut.Parameters.AddWithValue("@k1", TextBox1.Text);
+            komut.Parameters.AddWithValue("@k2", TextBox2.Text);
+            //kontrol edilen kısım
+            oku = komut.ExecuteReader();
+            // okunan veri uyuşuyor ise giriş için yönlendirme yapılacak
+            if (oku.Read())
+            {
+                Session["kullaniciadi"] = TextBox1.Text;
+                Response.Redirect("Yönlendirme yapmak istediğiniz link");
+                baglanti.Close();
+            }
+            // uyuşmuyor ise uyarı mesajı görünücek
+            else { Label3.Visible = true; }
+            
+        }
+    }
+}
